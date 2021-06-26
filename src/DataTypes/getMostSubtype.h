6161ed811960f72d8d20305186e61f639a2f59cc
vdimir <vdimir@yandex-team.ru>
2021-06-26 17:43:21 +0300

    Fix typo

--- src/DataTypes/getMostSubtype.h
+++ src/DataTypes/getMostSubtype.h
@@ -7,7 +7,7 @@
 namespace DB
 {
 
-/** Get data type that coverts intersection of all possible values of passed data types.
+/** Get data type that covers intersection of all possible values of passed data types.
   * DataTypeNothing is the most common subtype for all types.
   * Examples: most common subtype for UInt16, UInt8 and Int8 - UInt16.
   * Examples: most common subtype for Array(UInt8), Int8 is Nothing
